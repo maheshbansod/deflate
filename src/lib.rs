11b057265394162d10b22fb872f2085d6e4d6977
Mahesh Bansod <coolkevin011@gmail.com>
2025-03-07 23:18:35 +0530

    setup stuff for fixed tree compression

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,24 @@ use anyhow::Result;
 
 /// Compresses bits
 pub fn deflate(bytes: &[u8]) -> Result<Vec<u8>> {
-    // let's think of just one block for now
-    // so this block is the final block
-    // and let's go with the easiest case - no compression
-    let mut block = deflate_block_no_compression(bytes)?;
+    // arbitrary -> let's think on this later
+    let n_blocks = 2;
+    let block_size = bytes.len() / n_blocks;
+    let mut blocks = vec![];
+    let part1 = &bytes[0..block_size];
+    let block = deflate_block_no_compression(part1)?;
+    blocks.push(block);
+    let part2 = &bytes[block_size..];
+    let block = deflate_block_fixed_compression(part2)?;
+    blocks.push(block);
 
-    block[0] = block[0] | 0x80; //=> mark as final block
-    Ok(block)
+    let last_index = blocks.len() - 1;
+    blocks[last_index][0] = blocks[last_index][0] | 0x80; //=> mark as final block
+    Ok(blocks.into_iter().flatten().collect())
+}
+
+fn deflate_block_fixed_compression(bytes: &[u8]) -> Result<Vec<u8>> {
+    todo!()
 }
 
 fn deflate_block_no_compression(bytes: &[u8]) -> Result<Vec<u8>> {
@@ -36,23 +47,35 @@ const fn top_half_16(n: u16) -> u8 {
 }
 
 pub fn inflate(bytes: &[u8]) -> Result<Vec<u8>> {
-    let is_final_block = (bytes[0] & 0x80) != 0;
-    if !is_final_block {
-        todo!()
+    let mut output = vec![];
+    let mut bstart = 0;
+    loop {
+        let is_final_block = (bytes[bstart] & 0x80) != 0;
+        let btype = (bytes[bstart] & 0x60) >> 5;
+        let btype = get_btype(btype);
+        let remaining_bytes = &bytes[(bstart + 1)..];
+        let (consumed, block) = match btype {
+            Btype::NoCompression => inflate_no_compression(remaining_bytes)?,
+            Btype::CompressedFixed => inflate_block_fixed_compression(remaining_bytes)?,
+            _ => todo!(),
+        };
+        bstart += consumed;
+        output.extend_from_slice(&block);
+        if is_final_block {
+            break;
+        }
     }
-    let btype = (bytes[0] & 0x60) >> 5;
-    let btype = get_btype(btype);
-    let block = match btype {
-        Btype::NoCompression => inflate_no_compression(&bytes[1..])?,
-        _ => todo!(),
-    };
-    Ok(block.to_vec())
+    Ok(output)
 }
 
-fn inflate_no_compression(bytes: &[u8]) -> Result<Vec<u8>> {
+fn inflate_no_compression(bytes: &[u8]) -> Result<(usize, Vec<u8>)> {
     let len: usize = bytes[0] as usize * 256 + bytes[1] as usize;
     let data = &bytes[4..];
-    Ok(data[..len].to_vec())
+    Ok((len, data[..len].to_vec()))
+}
+
+fn inflate_block_fixed_compression(bytes: &[u8]) -> Result<(usize, Vec<u8>)> {
+    todo!()
 }
 
 enum Btype {
@@ -75,7 +98,10 @@ const fn get_btype(bits: u8) -> Btype {
 mod test {
     use anyhow::Result;
 
-    use crate::{deflate, deflate_block_no_compression, inflate};
+    use crate::{
+        deflate, deflate_block_fixed_compression, deflate_block_no_compression, inflate,
+        inflate_block_fixed_compression,
+    };
 
     #[test]
     fn no_compression() -> Result<()> {
@@ -84,6 +110,17 @@ mod test {
         let d = deflate_block_no_compression(s_bytes)?;
         let s2_bytes = &d[5..];
         assert_eq!(s_bytes, s2_bytes);
+        Ok(())
+    }
+
+    #[test]
+    fn fixed_compression() -> Result<()> {
+        let s = "a very normal string";
+        let s_bytes = s.as_bytes();
+        let d = deflate_block_fixed_compression(s_bytes)?;
+        let (consumed, s2_bytes) = inflate_block_fixed_compression(&d)?;
+        assert_eq!(s_bytes, s2_bytes);
+        assert_eq!(d.len(), consumed);
         Ok(())
     }
 
